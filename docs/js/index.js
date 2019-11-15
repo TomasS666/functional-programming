d6728981a42e800b60fa0919da224cfefc08333b
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2019-11-15 14:56:51 +0100

    added references

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,7 +3,7 @@ import { getData } from './helpers/fetchData.js';
 import { select, json, tsv, geoPath, geoMercator, zoom, event } from "d3";
 import { feature } from 'topojson';
 
-const url ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-38/sparql";
+const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-38/sparql";
 
 const query = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
@@ -38,125 +38,136 @@ getData(url, query).then(data => {
   console.log(data.results.bindings)
   initD3(data.results.bindings)
   console.log(data)
-	return data;
-}) 
+  return data;
+})
 
 
 
-function initD3(places){
+function initD3(places) {
 
-const svg = select('svg');
-const projection = geoMercator(); 
-const pathGenerator = geoPath().projection(projection);
-const g = svg.append('g');
-  
-// Config
-  
-const legendLineheight = 18;
-  
-  
-// Data
-  
-const cats = places.map((entry) => {
-  return entry.typeLabel.value;
-});
+// Reference to tutorial I followed https://www.youtube.com/watch?v=Qw6uAg3EO64&t=41s
 
-                     console.log(cats)
 
-  
-const colorscale = d3.scaleOrdinal()
-  .domain(cats)
-  .range(d3.schemeCategory10);
+  const svg = select('svg');
+  const projection = geoMercator();
+  const pathGenerator = geoPath().projection(projection);
+  const g = svg.append('g');
 
-g.append('path')
-  .attr('class', 'sphere')
-  .attr('d', pathGenerator({type: 'Sphere'}))
+  // Config
 
-	
-svg.call(zoom().on('zoom', () => {
-		g.attr('transform', event.transform);
-}));
- 
-  
+  const legendLineheight = 18;
+
+
+  // Data
+
+  const cats = places.map((entry) => {
+    return entry.typeLabel.value;
+  });
+
+  console.log(cats)
+
+
+  const colorscale = d3.scaleOrdinal()
+    .domain(cats)
+    .range(d3.schemeCategory10);
+
+  g.append('path')
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({
+      type: 'Sphere'
+    }))
+
+
+  svg.call(zoom().on('zoom', () => {
+    g.attr('transform', event.transform);
+  }));
+
+
+  // Legend: https://vizhub.com/Razpudding/921ee6d44b634067a2649f738b6a3a6e
+
   const legendWrapper = svg.append('g')
-  	.attr("class", "legend-group");
-  
+    .attr("class", "legend-group");
+
   legendWrapper.append("rect")
-        .attr("x", 5)
-				.attr("class", "legend-wrapper");
+    .attr("x", 5)
+    .attr("class", "legend-wrapper");
 
-  
+
   const legend = legendWrapper.selectAll(".legend")
-        .data(colorscale.domain())
-        .enter()
-  			.append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; })
+    .data(colorscale.domain())
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", function (d, i) {
+      return "translate(0," + i * 20 + ")";
+    })
 
 
-        legend.append("rect")
-        .attr("x", 185 - 18)
-        .attr("width", 18)
-        .attr("height", 18)
-        .style("fill", colorscale);
+  legend.append("rect")
+    .attr("x", 185 - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorscale);
 
-        legend.append("text")
-        .attr("x", 185 - 24)
-        .attr("y", 9)
-        .attr("dy", ".35em")
-  			.attr("class", "legend-text")
+  legend.append("text")
+    .attr("x", 185 - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .attr("class", "legend-text")
 
-        .text(function(d) { return d; });
-  
+    .text(function (d) {
+      return d;
+    });
+
   legendWrapper
-  .style("transform", "translate(-200px, 203px)");
+    .style("transform", "translate(-200px, 203px)");
   legendWrapper
-  	.transition().duration(2000)
-  		.style("transform", "translate(10px, 203px)");
+    .transition().duration(2000)
+    .style("transform", "translate(10px, 203px)");
 
 
-Promise.all([
-  tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
-  json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
-]).then(([tsvData, topoJSONdata]) => {
-  		const countryName = tsvData.reduce((accumulator, row) => {
-        
-  			accumulator[row.iso_n3] = row.name;
+  Promise.all([
+    tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+    json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+  ]).then(([tsvData, topoJSONdata]) => {
+    const countryName = tsvData.reduce((accumulator, row) => {
 
-      		return accumulator;
-      }, {});
-  
+      accumulator[row.iso_n3] = row.name;
 
-  
-			const country = feature(topoJSONdata, topoJSONdata.objects.countries);
-        g.selectAll('path')
-          .data(country.features)
-          .enter()
-          .append('path')
-          .attr('class', 'country')
-          .attr('d', d => pathGenerator(d))
-        .append('title')
-        	.text(d => countryName[d.id]);
-  
-  	g.selectAll('circle')
-  		.data(places)
-          .enter().append("circle")
-          .attr("r", 1)
-          .attr("transform", function(d) {
+      return accumulator;
+    }, {});
 
 
-          return "translate(" + projection([
-            d.long.value,
-            d.lat.value
-          ]) + ")";
-          })
-        .style('fill', function(d){
 
-          return colorscale(d.typeLabel.value)
-        })
-  			.append('text')
-  			.text(d => d.title.value)
-  					
-});
+    const country = feature(topoJSONdata, topoJSONdata.objects.countries);
+    g.selectAll('path')
+      .data(country.features)
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', d => pathGenerator(d))
+      .append('title')
+      .text(d => countryName[d.id]);
+
+    g.selectAll('circle')
+      .data(places)
+      .enter().append("circle")
+      .attr("r", 1)
+      .attr("transform", function (d) {
+
+
+        return "translate(" + projection([
+          d.long.value,
+          d.lat.value
+        ]) + ")";
+      })
+      .style('fill', function (d) {
+
+        return colorscale(d.typeLabel.value)
+      })
+      .append('text')
+      .text(d => d.title.value)
+
+  });
 
 }
